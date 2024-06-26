dac6cc605f44af72a99604331b922fa0a589d0b1
TushitPalamkar <143091687+TushitPalamkar@users.noreply.github.com>
2024-06-26 11:48:19 +0530

    Update auth.js

--- src/pages/auth.js
+++ src/pages/auth.js
@@ -19,7 +19,7 @@ const Login=()=>{
   const onSubmit=async(event)=>{
     event.preventDefault();
     try{
-      const response=await axios.post("http://localhost:3001/auth/login",{username,password})
+      const response=await axios.post("https://recipeapp-backend-rkvm.onrender.com/auth/login",{username,password})
       setCookies("access_token",response.data.token)
       window.localStorage.setItem("userID",response.data.userID)
       navigate("/")
@@ -39,7 +39,7 @@ const Register=()=>{
   const onSubmit=async(event)=>{
     event.preventDefault();
     try{
-      await axios.post("http://localhost:3001/auth/register",{username,password})
+      await axios.post("https://recipeapp-backend-rkvm.onrender.com/auth/register",{username,password})
       alert('Registration Completed! Now Login')
     }
     catch(err){
